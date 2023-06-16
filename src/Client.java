9eb0fc954f9727c646dd009927437403a7c58e5f
AA <Test@test.ru>
2023-06-16 14:57:04 +0300

    add comment

--- src/Client.java
+++ src/Client.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
+
+// add comment
 public class Client {
 	
 	public static void main(String[] args) {
