b7c2efb4f016899548843c49c483fcf45ed5b5f3
AA <Test@test.ru>
2023-06-16 14:18:42 +0300

    create socket for Client

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class Client {
-
+	
+	private static Socket clientSocket;
+	private static BufferedReader reader;
+    private static BufferedReader in; 
+    private static BufferedWriter out;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 clientSocket = new Socket("localhost", 9090);         
+         reader = new BufferedReader(new InputStreamReader(System.in));         
+         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));         
+         out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+         System.out.println("Введите текст");       
+         String word = reader.readLine(); 
+         
+         out.write(word + "\n");
+         out.flush();
+         String serverWord = in.readLine();
+         System.out.println(serverWord); 
 
 	}
 
