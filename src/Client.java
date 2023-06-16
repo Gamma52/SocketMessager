1eb694f25991117a01a5ca14c7128409b57ff07b
AA <Test@test.ru>
2023-06-16 14:35:50 +0300

    autoclose resources

--- src/Client.java
+++ src/Client.java
@@ -1,29 +1,31 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Client {
 	
-	private static Socket clientSocket;
-	private static BufferedReader reader;
-    private static BufferedReader in; 
-    private static BufferedWriter out;
-	
 	public static void main(String[] args) {
-		 clientSocket = new Socket("localhost", 9090);         
-         reader = new BufferedReader(new InputStreamReader(System.in));         
-         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));         
-         out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-         System.out.println("Введите текст");       
-         String word = reader.readLine(); 
-         
-         out.write(word + "\n");
-         out.flush();
-         String serverWord = in.readLine();
-         System.out.println(serverWord); 
+		try(
+			Socket clientSocket = new Socket("localhost", 4004);       
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));         
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));         
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+		    
+			){			 
+	
+	         System.out.println("Введите текст");       
+	         String word = reader.readLine(); 
+	         
+	         out.write(word + "\n");
+	         out.flush();
+	         String serverWord = in.readLine();
+	         System.out.println(serverWord);
+		}catch(IOException e) {
+			 System.err.println(e);
+		}
 
 	}
 
